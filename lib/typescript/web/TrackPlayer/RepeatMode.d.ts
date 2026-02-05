ae95a443902f4ac181c25f50dc183549dc41804d
Jesica <jestsee@gmail.com>
2026-02-05 16:40:40 +0700

    remove web related stuff

--- lib/typescript/web/TrackPlayer/RepeatMode.d.ts
+++
@@ -1,6 +0,0 @@
-export declare enum RepeatMode {
-    Off = 0,
-    Track = 1,
-    Playlist = 2
-}
-//# sourceMappingURL=RepeatMode.d.ts.map
--- lib/typescript/web/TrackPlayer/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './Player';
-export * from './PlaylistPlayer';
-export * from './RepeatMode';
-//# sourceMappingURL=index.d.ts.map
